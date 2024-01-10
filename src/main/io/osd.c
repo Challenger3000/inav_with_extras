bc2ca72b1a32b8e0d0a8845131bff5a274bd7d5a
Karlis Martinsons <k.martinsons@fixar.pro>
2024-01-11 01:37:39 +0200

    read serial attempt

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -1694,7 +1694,7 @@ void init_rerial_osd(void){
     portOptions_t osd_serial_portOptions = SERIAL_NOT_INVERTED | SERIAL_STOPBITS_1 | SERIAL_PARITY_NO;
 
     osd_serial_Port = openSerialPort(SERIAL_PORT_USART6, FUNCTION_UNUSED_3, NULL, NULL, baudRates[BAUD_115200], MODE_RXTX, osd_serial_portOptions);
-    strcpy(serial_text, "TEST MESSAGE EXAMPLE 2");
+    strcpy(serial_text, "TEST MESSAGE EXAMPLE  ");
     serialWrite(osd_serial_Port, 69);
     // blackboxWrite('T');
     // blackboxWrite('E');
@@ -1727,6 +1727,7 @@ static bool osdDrawSingleElement(uint8_t item)
             // char bufDISP[30];
             // strcpy(bufDISP, "Test message example");
             // bufDISP[29] = '\0';
+            serial_text[23] = serialRead(osd_serial_Port);
             displayWrite(osdDisplayPort, 1, 1, serial_text);
             serialWrite(osd_serial_Port, 0x69);
 
