f57f5994c2fe904f2a235b809130fbb0fb141acf
Karlis Martinsons <k.martinsons@fixar.pro>
2024-03-05 21:08:28 +0200

    testing crsfx

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -221,7 +221,8 @@ bool serialRxInit(const rxConfig_t *rxConfig, rxRuntimeConfig_t *rxRuntimeConfig
 #endif
 #ifdef USE_SERIALRX_CRSF
     case SERIALRX_CRSF:
-        enabled = crsfRxInit(rxConfig, rxRuntimeConfig);
+        // enabled = crsfRxInit(rxConfig, rxRuntimeConfig);
+        enabled = crsfRxInitx(rxConfig, rxRuntimeConfig);
         break;
 #endif
 #ifdef USE_SERIALRX_FPORT
