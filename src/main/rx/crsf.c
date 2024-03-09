4640f93bdc8dbd11be562f36f9dbdcb01e82a50a
Karlis Martinsons <k.martinsons@fixar.pro>
2024-03-09 16:08:10 +0200

    stock crsf?

--- src/main/rx/crsf.c
+++ src/main/rx/crsf.c
@@ -315,17 +315,17 @@ STATIC_UNIT_TESTED uint8_t crsfFrameStatus(rxRuntimeConfig_t *rxRuntimeConfig)
             crsfChannelData[14] = rcChannels->chan14;
             crsfChannelData[15] = rcChannels->chan15;
 
-            if(crsfChannelData[10] > 1600)
-            {
-                rx_kind = 1;
-                rxRuntimeConfigCopy->rcReadRawFn = functionPointer_1E;
-                rxRuntimeConfigCopy->rcFrameStatusFn = functionPointer_2E;
-            }else if(crsfChannelData[10] < 1400)
-            {
-                rx_kind = 0;
-                rxRuntimeConfigCopy->rcReadRawFn = functionPointer_1C;
-                rxRuntimeConfigCopy->rcFrameStatusFn = functionPointer_2C;
-            }
+            // if(crsfChannelData[10] > 1600)
+            // {
+            //     rx_kind = 1;
+            //     rxRuntimeConfigCopy->rcReadRawFn = functionPointer_1E;
+            //     rxRuntimeConfigCopy->rcFrameStatusFn = functionPointer_2E;
+            // }else if(crsfChannelData[10] < 1400)
+            // {
+            //     rx_kind = 0;
+            //     rxRuntimeConfigCopy->rcReadRawFn = functionPointer_1C;
+            //     rxRuntimeConfigCopy->rcFrameStatusFn = functionPointer_2C;
+            // }
             return RX_FRAME_COMPLETE;
         }
         else if (crsfFrame.frame.type == CRSF_FRAMETYPE_LINK_STATISTICS) {
