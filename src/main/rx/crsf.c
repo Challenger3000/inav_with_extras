1bc18ac7e85267151db27925037def292b1a629e
Karlis Martinsons <k.martinsons@fixar.pro>
2024-03-11 21:29:50 +0200

    WTF HUINA

--- src/main/rx/crsf.c
+++ src/main/rx/crsf.c
@@ -616,11 +616,11 @@ STATIC_UNIT_TESTED uint8_t crsfFrameStatus(rxRuntimeConfig_t *rxRuntimeConfig)
 
 uint8_t status_frame_manger(rxRuntimeConfig_t *rxRuntimeConfig){
 
-    if(micros() > 20000000){
-        char str[12]; // Buffer big enough for an integer
-        itoa(rx_kind, str, 10); // 10 is the base for decimal numbers
-        cliPrint(str);
-    }
+    // if(micros() > 20000000){
+    //     char str[12]; // Buffer big enough for an integer
+    //     itoa(rx_kind, str, 10); // 10 is the base for decimal numbers
+    //     cliPrint(str);
+    // }
 
     if(rx_kind == 1){
         crsfFrameStatus(&rxRuntimeConfig);
