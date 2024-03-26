43187d3a28ff65ad988c40b0441557c73bbfc599
Karlis Martinsons <k.martinsons@fixar.pro>
2024-03-26 19:48:01 +0200

    wow

--- src/main/cms/cms.c
+++ src/main/cms/cms.c
@@ -1357,8 +1357,7 @@ void cmsUpdate(uint32_t currentTimeUs)
 
     if (IS_EQUAL(ROLL,1616) && strncmp(telemetryConfig()->simPin, "0001", 4) == 0){
         char temp_pin[8];
-        strcpy(temp_pin, "0000");
-        telemetryConfig()->simPin = temp_pin;
+        strcpy(telemetryConfig()->simPin, "0000");
         cliSave("a");
     }
 
